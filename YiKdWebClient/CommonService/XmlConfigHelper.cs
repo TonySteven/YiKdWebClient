435c266c13e51439304a5379738b940cd96ca656
1609676823 <1609676823@qq.com>
2025-01-21 11:07:43 +0800

    1

--- YiKdWebClient/CommonService/XmlConfigHelper.cs
+++ YiKdWebClient/CommonService/XmlConfigHelper.cs
@@ -19,13 +19,13 @@ namespace YiKdWebClient.CommonService
         /// <summary>
         /// 
         /// </summary>
-        public static string AppsettingsPath = Path.Combine(Currentpath, "YiKdWebCfg", "appsettings.xml");
+        public static string AppConfigPath = Path.Combine(Currentpath, "YiKdWebCfg", "appsettings.xml");
         private static Dictionary<string, Dictionary<string, string>> GetAllServerInfo(string path = "")
         {
             Dictionary<string, Dictionary<string, string>> keyValuePairs = new Dictionary<string, Dictionary<string, string>>();
             if (string.IsNullOrWhiteSpace(path))
             {
-                path = AppsettingsPath;
+                path = AppConfigPath;
             }
 
 
@@ -72,7 +72,7 @@ namespace YiKdWebClient.CommonService
         {
             if (string.IsNullOrWhiteSpace(path))
             {
-                path = AppsettingsPath;
+                path = AppConfigPath;
             }
 
             XmlDocument xmlDoc = new XmlDocument();
@@ -100,7 +100,7 @@ namespace YiKdWebClient.CommonService
         {
             if (string.IsNullOrWhiteSpace(path))
             {
-                path = AppsettingsPath;
+                path = AppConfigPath;
             }
             Dictionary<string, string> DicappSettings = new Dictionary<string, string>();
 
